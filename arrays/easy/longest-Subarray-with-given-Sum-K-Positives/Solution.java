c4c789d15ca6f3aaa18176fd2260cb0e93630f19
Vigneshwar-MS <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-09-22 09:52:42 +0530

    Add files via upload

---
+++ arrays/easy/longest-Subarray-with-given-Sum-K-Positives/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+
+
+    public static int getLongestSubarrayWithOnlyPositiveElements(int[] a, int k) {
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getLongestSubarrayWithOnlyPositiveElements(new int[]{2, 3, 5, 1, 9}, 10));
+    }
+}
+
+//Time Complexity is ~ O(N)
+//Space Complexity is ~ O(1)
