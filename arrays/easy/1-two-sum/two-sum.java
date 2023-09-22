014546cb8d9bbd491a03a91632d21c9226e72f56
Vigneshwar-MS <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-09-22 12:45:20 +0530

    Time: 1 ms (99.38%) | Memory: 43.5 MB (71.83%) - LeetSync

--- arrays/easy/1-two-sum/two-sum.java
+++ arrays/easy/1-two-sum/two-sum.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < len; i++){
-            for(int j = i + 1; j < len; j++){
-                if(nums[i] + nums[j] == target) return new int[]{i, j};
-            }
+            
+            int secondVal = target - nums[i];
+            
+            if(map.containsKey(secondVal)) return new int[]{map.get(secondVal), i};
+            
+            map.put(nums[i], i);
         }
         return new int[]{-1, -1};
     }
 }
 
-//Time complexity is O(N^2)
-//Space complexity is O(1)
+//Time complexity is O(NlogN)
+//Space complexity is O(N)
