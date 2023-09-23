6d3824497c1fe8c6cecf45512969dd43eb855d80
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-09-23 20:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (38.48%) - LeetSync

---
+++ binarysearch/easy/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int left = 0, right = len - 1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target){
+                    return mid;
+            }
+
+            if(nums[mid] >= nums[left]){
+                if(target >= nums[left] && target <= nums[mid]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(target >= nums[mid] && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+//Time complexity is O(log N)
+//Space complexity is O(1)
