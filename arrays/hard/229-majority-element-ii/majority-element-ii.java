5375930026e2d049093308ef10284ab687ab6bd1
Vigneshwar-MS <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-09-23 09:02:18 +0530

    Time: 2 ms (98.45%) | Memory: 46.8 MB (41.15%) - LeetSync

--- arrays/hard/229-majority-element-ii/majority-element-ii.java
+++ arrays/hard/229-majority-element-ii/majority-element-ii.java
@@ -1,27 +1,50 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        int ele1 = Integer.MIN_VALUE, ele1Cnt = 0;
+        int ele2 = Integer.MIN_VALUE, ele2Cnt = 0;
         int len = nums.length;
         List<Integer> ans = new ArrayList<>();
 
         for(int i = 0; i < len; i++){
-            if(ans.size() == 0 || ans.get(0) != nums[i]){
-                int count = 0;
-                for(int j = 0; j < len; j++){
-                    if(nums[i] == nums[j]){
-                        count++;
-                    }
-                }
-                if(count > len / 3){
-                    ans.add(nums[i]);
-                }
+            if(ele1Cnt == 0 && nums[i] != ele2){
+                ele1 = nums[i];
+                ele1Cnt++;
             }
-            
-            if(ans.size() == 2){
-                break;
+            else if(ele2Cnt == 0 && nums[i] != ele1){
+                ele2 = nums[i];
+                ele2Cnt++;
+            }
+            else if(nums[i] == ele1){
+                ele1Cnt++;
+            }
+            else if(nums[i] == ele2){
+                ele2Cnt++;
+            }
+            else{
+                ele1Cnt--;
+                ele2Cnt--;
             }
         }
-
+        if(checkMajority(nums, ele1)) ans.add(ele1);
+        if(checkMajority(nums, ele2)) ans.add(ele2);
         return ans;
     }
+
+    static boolean checkMajority(int[] nums, int val){
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == val){
+                count++;
+            }
+        }
+        if(count > (nums.length / 3 )){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
+
+//Time Complexity is O(N)
+//Space Complexity is O(1)
