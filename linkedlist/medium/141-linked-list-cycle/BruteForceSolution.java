2ca1b2c896c94b6940edc8635f75ae04fef3d88a
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-09-26 19:50:32 +0530

    Add files via upload

---
+++ linkedlist/medium/141-linked-list-cycle/BruteForceSolution.java
@@ -0,0 +1,25 @@
+class RecursiveSolution {
+
+    //Source - https://leetcode.com/problems/linked-list-cycle/description/
+    //Tag - Linked List
+
+    public boolean hasCycle(ListNode head) {
+
+        Set<ListNode> set = new HashSet<>();
+
+        while(head != null){
+
+            if(set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+
+        return false;
+    }
+
+}
+
+//Time Complexity is O(N)
+//Space Complexity is O(N)
