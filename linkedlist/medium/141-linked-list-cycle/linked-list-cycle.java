251d3b141055945809f0e7822621d1ae2239d473
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-09-26 19:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (58.12%) - LeetSync

---
+++ linkedlist/medium/141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        ListNode first = head, second = head;
+        while(second != null && second.next != null){
+            first = first.next;
+            second = second.next.next;
+            if(first == second){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+//Time Complexity is O(N)
+//Space Complexity is O(1)
