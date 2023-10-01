316c90108755855c4ba1a3c7868a2517eaaa6d18
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-10-01 15:32:00 +0530

    Time: 6 ms (93.80%) | Memory: 41.7 MB (51.42%) - LeetSync

--- strings/medium/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ strings/medium/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,16 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int ans = 0, left = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        int[] map = new int[26];
         
         for(int i = 0; i < s.length(); i++){
             
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map[s.charAt(i) - 'A']++;
+            int max = Integer.MIN_VALUE;
             
-            while((i - left + 1) - Collections.max(map.values()) > k){
-                map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) - 1);
+            for(int j = 0; j < 26; j++){
+                max = Math.max(max, map[j]);
+            }
+
+            while((i - left + 1) - max > k){
+                map[s.charAt(left) - 'A']--;
                 left++;
             }
+
             ans = Math.max(ans, i - left + 1);
         }
         return ans;
