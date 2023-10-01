6e904b7e9d2cc66139397cbf13161e61c9a32509
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-10-01 11:02:27 +0530

    Time: 3 ms (90.47%) | Memory: 43.5 MB (53.12%) - LeetSync

--- strings/easy/242-valid-anagram/valid-anagram.java
+++ strings/easy/242-valid-anagram/valid-anagram.java
@@ -1,25 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        int len1 = s.length(), len2 = t.length();
-        int[] map = new int[26];
 
-        for(int i = 0; i < len1; i++){
-            map[s.charAt(i) - 'a']++;
-        }
-
-        for(int i = 0; i < len2; i++){
-            map[t.charAt(i) - 'a']--;
-        }
-
-        for(int i = 0; i < 26; i++){
-            if(map[i] != 0) return false;
-        }
-
-
-        return true;
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        return new String(ch1).equals(new String(ch2));
 
     }
 }
-//Time Complexity :  O(N).  where N is the length of each string. 
+//Time Complexity :  O(NlogN).  where N is the length of each string. 
 //Space complexity is O(1).
