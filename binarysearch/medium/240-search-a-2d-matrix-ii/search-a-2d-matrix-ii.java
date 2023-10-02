384bb837b8bf366af49bc0a4b2e6aa8c5c16e278
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-10-02 13:45:24 +0530

    Time: 12 ms (7.39%) | Memory: 49 MB (52.98%) - LeetSync

---
+++ binarysearch/medium/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        int row = matrix.length, col = matrix[0].length;
+    
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(matrix[i][j] == target) return true;
+            }
+        }
+        return false;
+    }
+}
+
+//Time Complexity is O(M * N), where M is number of rows and N is number of columns
+//Space Complexity is O(1)
