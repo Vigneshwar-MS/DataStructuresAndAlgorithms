636338fb1c25f6b821f7ea26d40a69916c60f479
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-10-02 13:47:22 +0530

    Time: 6 ms (30.05%) | Memory: 49.2 MB (30.99%) - LeetSync

--- binarysearch/medium/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ binarysearch/medium/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -4,13 +4,19 @@ class Solution {
         int row = matrix.length, col = matrix[0].length;
     
         for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(matrix[i][j] == target) return true;
+            int left = 0, right = col - 1;
+            while(left <= right){
+                int mid = (left + right) / 2;
+                if(matrix[i][mid] == target) return true;
+                else if(matrix[i][mid] < target) left = mid + 1;
+                else{
+                    right = mid - 1;
+                }
             }
         }
         return false;
     }
 }
 
-//Time Complexity is O(M * N), where M is number of rows and N is number of columns
+//Time Complexity is O(M * logN), where M is number of rows and N is number of columns
 //Space Complexity is O(1)
