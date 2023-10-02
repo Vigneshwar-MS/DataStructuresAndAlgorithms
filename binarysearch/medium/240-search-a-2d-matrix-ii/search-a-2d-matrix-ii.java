a7089f615c41256b883093eb2b0f6a68c36321e7
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-10-02 13:48:08 +0530

    Time: 11 ms (11.99%) | Memory: 48.6 MB (95.46%) - LeetSync

--- binarysearch/medium/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ binarysearch/medium/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -4,14 +4,8 @@ class Solution {
         int row = matrix.length, col = matrix[0].length;
     
         for(int i = 0; i < row; i++){
-            int left = 0, right = col - 1;
-            while(left <= right){
-                int mid = (left + right) / 2;
-                if(matrix[i][mid] == target) return true;
-                else if(matrix[i][mid] < target) left = mid + 1;
-                else{
-                    right = mid - 1;
-                }
+            for(int j = 0; j < col; j++){
+                if(matrix[i][j] == target) return true;
             }
         }
         return false;
