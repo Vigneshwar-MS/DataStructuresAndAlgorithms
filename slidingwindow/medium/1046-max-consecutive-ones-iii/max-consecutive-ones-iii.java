aa2b72e6a19ff075175d5a1fae5e1bb90d1f2a9e
vigneshwar_ms <65734220+Vigneshwar-MS@users.noreply.github.com>
2023-10-02 22:04:40 +0530

    Time: 4 ms (21.61%) | Memory: 46.6 MB (14.29%) - LeetSync

--- slidingwindow/medium/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ slidingwindow/medium/1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -7,13 +7,10 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             
             map[nums[i]]++;
-            int max = Math.max(map[0], map[1]);
-
             while( (i - left + 1) - map[1] > k){
                 map[nums[left]]--;
                 left++;
             }
-
             res = Math.max(res, i - left + 1);
         }
         return res;
